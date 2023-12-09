0fc5a44380ba356f46c32310944702dbdc614e83
Udo Nkem <udonkem@gmail.com>
2023-12-09 14:52:41 +0100

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -53,6 +53,13 @@ def main():
         diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
     
     st.success(diagnosis)
-    
+
+import streamlit as st
+
+video_file = open('myvideo.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
 if __name__=='__main__':
     main()
