e6a39f51ad345442dae882155dda845bd63e9ed1
Udo Nkem <udonkem@gmail.com>
2024-05-29 13:22:12 +0100

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -6,7 +6,7 @@ loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 # reshape the array as we are predicting for one instance
-     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+      input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
     prediction = loaded_model.predict(input_data_reshaped)
     print(prediction)
