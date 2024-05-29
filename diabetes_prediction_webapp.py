3fdeebf351768f169b268c9a54a7b53f62b5ef26
Udo Nkem <udonkem@gmail.com>
2024-05-29 17:27:29 +0100

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -42,20 +42,7 @@ footer:after {
 }
             </style>
             """
-	page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
-background-size: cover;
-background-position: center center;
-background-repeat: no-repeat;
-background-attachment: local;
-}}
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-</style>
-"""
+	
     # giving title
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     st.markdown(hide_streamlit_style2, unsafe_allow_html=True)
