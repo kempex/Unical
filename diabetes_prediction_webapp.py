6e6f66bb040ca18399f562c89ca9f8524f2d4f9c
Udo Nkem <udonkem@gmail.com>
2024-05-29 13:26:39 +0100

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -2,22 +2,24 @@ import numpy as np
 import pickle
 import streamlit as st
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
 # creating a function for prediction
-# changing the input_data to numpy array
+def diabetes_prediction(input_data):
+    
+
+    # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
-# reshape the array as we are predicting for one instance
-      input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
     prediction = loaded_model.predict(input_data_reshaped)
     print(prediction)
 
     if (prediction[0] == 0):
-        #st.toast('You are not diabetic', icon='😍')
-      return 'You are not diabetic'
-       
-        
+      return 'The person is not diabetic'
     else:
-      return 'You are diabetic'
+      return 'The person is diabetic'
      
 def main():
     
